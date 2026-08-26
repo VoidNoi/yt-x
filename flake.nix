7292e41466f3145f3897bb74abe3b8cc930222cf
VoidNoi <voidnoi@proton.me>
2026-08-26 18:51:16 +0200

    Update flake.lock & change to stable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Browse YouTube from your terminal with yt-x";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
